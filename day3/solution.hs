72398572e06e0dd4c553f0d30f4bd9a641730e2e
Jonathan Lalande <lalande.gg@gmail.com>
2019-02-09 15:39:47 -0500

    add day3 p1 and p2

--- day3/solution.hs
+++ day3/solution.hs
@@ -1,10 +1,13 @@
+import qualified Data.Set as S
+import qualified Data.Map as M
+
 data Direction = N | E | S | W deriving (Show)
 
-directionToVec :: Direction -> (Int, Int)
-directionToVec N = (0, 1)
-directionToVec E = (1, 0)
-directionToVec S = (0, -1)
-directionToVec W = (-1, 0)
+move :: Direction -> (Int, Int) -> (Int, Int)
+move N (x, y) = (x, y + 1)
+move E (x, y) = (x + 1, y)
+move S (x, y) = (x, y - 1)
+move W (x, y) = (x - 1, y)
 
 left :: Direction -> Direction
 left N = W
@@ -12,28 +15,46 @@ left E = N
 left S = E
 left W = S
 
-vadd :: (Int, Int) -> (Int, Int) -> (Int, Int)
-vadd (x,y) (i, j) = (x + i, y + j)
-
 vdist :: (Int, Int) -> (Int, Int) -> Int
 vdist (x, y) (i, j) = abs (x - i) + abs (y - j)
 
--- return the positions walked on from walking `n` step in `dir` direction
-walk :: (Int, Int) -> Int -> Direction -> [(Int, Int)]
-walk start n dir = init . scanr f start . take n $ repeat dir
+-- turn left, whenever the left position from current position has not been seen
+spiral :: [(Int, Int)]
+spiral = map (\(p, _, _) -> p) $ iterate next ((0, 0), S, S.singleton (0, 0))
   where
-    f dir pos = vadd pos (directionToVec dir)
+    next (pos, dir, seen) =
+      if (move (left dir) pos) `S.notMember` seen
+        then (move (left dir) pos, left dir, S.insert (move (left dir) pos) seen)
+        else (move dir pos, dir, S.insert (move dir pos) seen)
 
-spiral :: Int -> [(Int, Int)]
-spiral n = take n . reverse . snd . foldl fold (E, [(0, 0)]) $ moves [1..n]
+neighbors :: (Int, Int) -> [(Int, Int)]
+neighbors (x, y) =
+  [
+    (x + 1, y)
+  , (x + 1, y + 1)
+  , (x, y + 1)
+  , (x - 1, y + 1)
+  , (x - 1, y)
+  , (x - 1, y - 1)
+  , (x, y - 1)
+  , (x + 1, y - 1)
+  ]
+
+-- sumspiral :: [Int]
+sumspiral = f M.empty spiral
   where
-    fold = (\(dir, (p:ps)) n -> (left dir, (walk p n dir) ++ (p:ps)))
-    moves = concat . map (\x -> [x, x])
+    f sums ((0,0):ps) = 1 : f (M.insert (0, 0) 1 sums) ps
+    f sums (p:ps) = val : f (M.insert p val sums) ps
+      where
+        val = sum $ sum <$> (`M.lookup` sums) <$> (neighbors p)
 
-part1 n = vdist (0, 0) . last $ spiral n
+part1 :: Int -> Int
+part1 n = vdist (0, 0) $ spiral !! (n-1)
 
+part2 n = head . dropWhile (< n) $ sumspiral
 
 main = do
   content <- readFile "input.txt"
   let input = read content :: Int
-  putStrLn $ show $ part1 input
+  putStrLn $ "Part1: " ++ (show $ part1 input)
+  putStrLn $ "Part2: " ++ (show $ part2 input)
