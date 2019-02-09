64668651bc07125629b199b95403c33f3b0b0865
Jonathan Lalande <lalande.gg@gmail.com>
2019-02-09 15:59:19 -0500

    day4 p2

--- day4/solution.hs
+++ day4/solution.hs
@@ -5,10 +5,19 @@ isValid :: String -> Bool
 isValid passphrase = (nub ws) == ws
   where ws = words passphrase
 
+isValid2 :: String -> Bool
+isValid2 passphrase = (nub ws) == ws
+  where ws = map sort . words $ passphrase
+
+
 part1 :: [String] -> Int
 part1 = length . filter isValid
+
+part2 :: [String] -> Int
+part2 = length . filter isValid2
 
 main = do
   content <- readFile "input.txt"
   let input = lines . strip $ content
   putStrLn $ "Part1: " ++ (show $ part1 input)
+  putStrLn $ "Part2: " ++ (show $ part2 input)
